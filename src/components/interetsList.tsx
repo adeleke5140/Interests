58cf26f1eb397b85519501cc7316a79c520ae8d0
Kenny <adelekekehinde06@gmail.com>
2023-07-18 00:37:56 +0100

    feat: major features

--- src/components/interetsList.tsx
+++ src/components/interetsList.tsx
@@ -1,9 +1,9 @@
-import { Box, Card, CardContent, Chip, Skeleton, Stack, ThemeProvider, Typography } from "@mui/material"
-import { useGetIdentity, useList, useOne } from "@refinedev/core"
+import { Box, Button, Card, CardContent, Chip, Skeleton, Stack, ThemeProvider, Typography } from "@mui/material"
+import { BaseKey, useDelete, useGetIdentity, useList, useOne } from "@refinedev/core"
 import { useColorMode } from "contexts/color-mode"
 import { headerTheme } from "contexts/theme"
 import { IUser } from "pages/home"
-import { useEffect, useState } from "react"
+import InterestsIcon from '@mui/icons-material/Interests';
 
 const interests = [
   {
@@ -28,6 +28,9 @@ const InterestsList = () => {
   const { data: user } = useGetIdentity<IUser>()
   const { mode } = useColorMode()
 
+  const { mutate: deleteInterest } = useDelete()
+
+
   const { data, isLoading, isError } = useList({
     resource: 'interests',
     filters: [
@@ -38,6 +41,25 @@ const InterestsList = () => {
       }
     ]
   })
+
+  function handleDeleteInterest(id: BaseKey) {
+    deleteInterest({
+      resource: 'interests',
+      id,
+      successNotification: (data, id, resource) => {
+        return {
+          type: 'success',
+          message: 'Interest deleted!'
+        }
+      },
+      errorNotification: (data, id, resource) => {
+        return {
+          type: 'error',
+          message: 'Could not delete interest, try again.'
+        }
+      }
+    })
+  }
 
   const interests = data?.data ?? [] as InterestsType
 
@@ -66,7 +88,7 @@ const InterestsList = () => {
       <Box sx={{ paddingTop: '1em', paddingLeft: '1em', paddingRight: '1em' }}>
         <ThemeProvider theme={headerTheme}>
           <Typography sx={{ fontSize: '1.5rem', fontWeight: '600', mb: 2 }}>
-            Your Saved Interests
+            Your Saved <Chip color="primary" icon={<InterestsIcon />} size="medium" label="Interests" />
           </Typography>
         </ThemeProvider>
         <Stack spacing={2}>
@@ -80,7 +102,7 @@ const InterestsList = () => {
     <Box sx={{ paddingTop: '1em', paddingLeft: '1em', paddingRight: '1em' }}>
       <ThemeProvider theme={headerTheme}>
         <Typography sx={{ fontSize: '1.5rem', fontWeight: '600', mb: 2 }}>
-          Your Saved Interests
+          Your Saved <Chip color="primary" icon={<InterestsIcon />} size="medium" label="Interests" />
         </Typography>
       </ThemeProvider>
       <Stack spacing={2}>
@@ -88,27 +110,63 @@ const InterestsList = () => {
           <Card key={interest.name} sx={{
             backgroundColor: `${mode === 'dark' ? 'unset' : ""}`,
             border: `${mode === 'dark' ? '1px solid' : ""}`,
+            position: 'relative',
+            borderRadius: '20px'
           }}>
             <CardContent>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography sx={{ fontSize: 18, textTransform: 'capitalize' }} color="text.primary" gutterBottom>
-                  {interest.name}
-                </Typography>
-                <Chip color="primary" label={interest.activity} />
+              <Stack sx={{
+                position: 'relative'
+              }} direction="row" justifyContent="space-between">
+                <Stack direction="row" spacing={2}>
+                  <Typography sx={{
+                    fontSize: {
+                      sm: 10,
+                      lg: 18
+                    }, textTransform: 'capitalize'
+                  }} color="text.primary" gutterBottom>
+                    {interest.name}
+                  </Typography>
+                  <Chip size="small" color="primary" label={interest.activity} />
+                </Stack>
+                <Button sx={{
+                  width: '0.8rem',
+                  positon: 'absolute',
+                  position: {
+                    xs: 'absolute',
+                    lg: 'relative',
+                  },
+                  bottom: {
+                    xs: -40,
+                    lg: 0
+                  },
+                  right: 0
+                }} size="small" aria-label="Delete" variant="contained" onClick={() => handleDeleteInterest(interest.id!)} >Delete</Button>
               </Stack>
               <Typography sx={{
-                color: `${mode === 'dark' ? "#5baffaf" : "rgba(0, 0, 0, 0.6)"}`
-              }} color="text.secondary">
-                Date: {interest.date}
+                color: `${mode === 'dark' ? "#fff" : "#000"}`,
+                mb: 1
+              }} color="text.primary">
+                Category:
+                <Typography sx={{
+                  color: `${mode === 'dark' ? "#5baffa" : "#000"}`
+                }} component="span"> {interest.category}</Typography>
+              </Typography>
+              <Typography sx={{
+                color: `${mode === 'dark' ? "#fff" : "#000"}`
+              }} color="text.primary">
+                Date:
+                <Typography sx={{
+                  color: `${mode === 'dark' ? "#5baffa" : "#000"}`
+                }} component="span"> {interest.date}</Typography>
               </Typography>
             </CardContent>
           </Card>
         ))}
       </Stack>
       <Stack textAlign="center" sx={{ mt: 2 }}>
-        <Typography>Load More</Typography>
+        <Typography>That's all 🎉</Typography>
       </Stack>
-    </Box>
+    </Box >
   )
 }
 export { InterestsList }
--- src/components/userProfile.tsx
+++ src/components/userProfile.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Divider, Stack, ThemeProvider, Typography } from "@mui/mat
 import { useGetIdentity, useLogout } from "@refinedev/core"
 import { useColorMode } from "contexts/color-mode"
 import { headerTheme } from "contexts/theme"
+import Person4Icon from '@mui/icons-material/Person4';
 
 import type { IUser } from "pages/home"
 
@@ -10,22 +11,35 @@ const UserProfile = () => {
   const { mutate: logout } = useLogout()
   const { data: user } = useGetIdentity<IUser>()
   return (
-    <Box sx={{ paddingTop: '1em', paddingLeft: '1em', paddingRight: '1em' }}>
+    <Box sx={{ paddingTop: '1em', paddingLeft: '1em', paddingRight: '1em', position: 'relative' }}>
       <ThemeProvider theme={headerTheme}>
-        <Typography sx={{ fontSize: '1.5rem', fontWeight: '600', mb: 2 }}>
-          Profile
-        </Typography>
+        <Stack gap={2} direction="row">
+          <Box sx={{
+            position: 'relative',
+            top: 6
+          }}>
+            <Person4Icon />
+          </Box>
+          <Typography sx={{ fontSize: '1.5rem', fontWeight: '600', mb: 2 }}>
+            Profile
+          </Typography>
+
+        </Stack>
       </ThemeProvider>
       <Stack spacing={2}>
         <Typography>Email: {user?.email}</Typography>
         <Divider light={true} sx={{
           borderColor: `${mode === 'dark' ? "#5baffa" : "rgba(0, 0, 0, 0.6)"}`,
         }} />
+        <Typography>
+          Surpised by the lack of options? It is intentional.
+          We are committed to be as clutter free as possible.
+        </Typography>
         <Stack alignItems="flex-end">
           <Button onClick={() => logout()} variant="contained" color="error">Log out</Button>
         </Stack>
       </Stack>
-    </Box>
+    </Box >
   )
 }
 
