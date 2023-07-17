f01cc10ec2a22f2e08b7bd548afc1b5ce9681512
Kenny <adelekekehinde06@gmail.com>
2023-07-17 21:27:35 +0100

    feat: get user details

--- src/components/userProfile.tsx
+++ src/components/userProfile.tsx
@@ -1,9 +1,14 @@
 import { Box, Button, Divider, Stack, ThemeProvider, Typography } from "@mui/material"
+import { useGetIdentity, useLogout } from "@refinedev/core"
 import { useColorMode } from "contexts/color-mode"
 import { headerTheme } from "contexts/theme"
 
+import type { IUser } from "pages/home"
+
 const UserProfile = () => {
   const { mode } = useColorMode()
+  const { mutate: logout } = useLogout()
+  const { data: user } = useGetIdentity<IUser>()
   return (
     <Box sx={{ paddingTop: '1em', paddingLeft: '1em', paddingRight: '1em' }}>
       <ThemeProvider theme={headerTheme}>
@@ -12,17 +17,12 @@ const UserProfile = () => {
         </Typography>
       </ThemeProvider>
       <Stack spacing={2}>
-        <Typography>Name: Kehinde Adeleke</Typography>
-        <Typography>Email: adelekekehinde06@gmail.com</Typography>
+        <Typography>Email: {user?.email}</Typography>
         <Divider light={true} sx={{
           borderColor: `${mode === 'dark' ? "#5baffa" : "rgba(0, 0, 0, 0.6)"}`,
         }} />
         <Stack alignItems="flex-end">
-          <Typography>
-            We take your data privacy seriously. If you don&apos;t feel like using the app again,
-            you can delete your account.
-          </Typography>
-          <Button variant="contained" color="error">Delete Account</Button>
+          <Button onClick={() => logout()} variant="contained" color="error">Log out</Button>
         </Stack>
       </Stack>
     </Box>
