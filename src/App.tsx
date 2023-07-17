f4415a38c74297b551550353eb0674881995bc36
Kenny <adelekekehinde06@gmail.com>
2023-07-17 21:26:13 +0100

    feat: add authenticated route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Refine } from "@refinedev/core";
+import { Refine, Authenticated } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { ErrorComponent, RefineSnackbarProvider } from "@refinedev/mui";
+import { ErrorComponent, RefineSnackbarProvider, AuthPage } from "@refinedev/mui";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
@@ -10,7 +10,7 @@ import routerBindings, {
   UnsavedChangesNotifier
 } from "@refinedev/react-router-v6";
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 
 // import { MuiInferencer } from '@refinedev/inferencer/mui'
@@ -32,7 +32,12 @@ import ProfilePage from "pages/profile";
 import InterestsPage from "pages/interests";
 import { Layout } from "components/layout";
 
-
+import routerProvider, {
+  CatchAllNavigate,
+  NavigateToResource
+} from '@refinedev/react-router-v6'
+import { Header } from "components";
+import { Box, Typography } from "@mui/material";
 function App() {
   return (
     <BrowserRouter>
@@ -42,39 +47,118 @@ function App() {
           <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
           <RefineSnackbarProvider>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Refine
-                authProvider={authProvider}
-                notificationProvider={notificationProvider}
-                routerProvider={routerBindings}
-                dataProvider={dataProvider(supabaseClient)}
-                liveProvider={liveProvider(supabaseClient)}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                }}
+              <ThemeProvider theme={theme}>
+                <Refine
+                  authProvider={authProvider}
+                  notificationProvider={notificationProvider}
+                  routerProvider={routerBindings}
+                  dataProvider={dataProvider(supabaseClient)}
+                  liveProvider={liveProvider(supabaseClient)}
+                  options={{
+                    syncWithLocation: true,
+                    warnWhenUnsavedChanges: true,
+                  }}
+                  resources={[
+                    {
+                      name: 'interests',
+                      list: '/interests',
+                      create: '/create',
+                      show: '/profile'
+                    }
+                  ]}
+                >
 
-              >
-                <ThemeProvider theme={theme}>
                   <Routes>
                     <Route index element={<Index />} />
-                    <Route element={<Layout />}>
-                      <Route path="/home" element={<HomePage />} />
+                    <Route element={(
+                      <Authenticated redirectOnFail="/login">
+                        <Layout />
+                      </Authenticated>
+                    )}
+                    >
+                      <Route path="/create" element={<HomePage />} />
                       <Route path="/interests" element={<InterestsPage />} />
                       <Route path="/profile" element={<ProfilePage />} />
                     </Route>
+
+
+                    <Route element={
+                      <Authenticated fallback={<Outlet />}>
+                        <NavigateToResource resource="interests" />
+                      </Authenticated>
+                    }>
+                      <Route
+                        path="/login"
+                        element={
+                          <div style={{
+                            marginTop: '-4rem'
+                          }}>
+                            <Header />
+                            <AuthPage
+                              title={
+                                <h3 style={{
+                                  color: '#5baffa',
+                                  fontFamily: 'Raleway Variable',
+                                  marginBottom: 0,
+
+                                }}>Interests</h3>}
+                              type="login"
+                            />
+                            <Box sx={{ textAlign: 'center', mt: -4 }}>
+                              <Typography>© No rights reserved -
+                                <a
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  href='https://github.com/adeleke5140'
+                                  style={{
+                                    color: '#5baffa'
+                                  }}>
+                                  adeleke5140
+                                </a></Typography>
+                            </Box>
+                          </div>
+                        }
+                      />
+                      <Route
+                        path="/register"
+                        element={
+                          <div>
+                            <Header />
+                            <AuthPage title={
+                              <h3 style={{
+                                color: '#5baffa',
+                                fontFamily: 'Raleway Variable',
+                                marginBottom: 0
+                              }}>Interests</h3>} type="register" />
+                          </div>
+                        }
+                      />
+                    </Route>
+                    <Route
+                      path="/forgot-password"
+                      element={
+                        <AuthPage title={
+                          <h3 style={{
+                            color: '#5baffa',
+                            fontFamily: 'Raleway Variable',
+                            marginBottom: 0
+                          }}>Interests</h3>} type="forgotPassword" />}
+                    />
+
                     <Route path="*" element={<ErrorComponent />} />
                   </Routes>
-                </ThemeProvider>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-                <Toaster />
-              </Refine>
+
+                  <RefineKbar />
+                  <UnsavedChangesNotifier />
+                  <DocumentTitleHandler />
+                  <Toaster />
+                </Refine>
+              </ThemeProvider>
             </LocalizationProvider>
           </RefineSnackbarProvider>
         </ColorModeContextProvider>
       </RefineKbarProvider>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
