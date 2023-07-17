cea76d785acd45873788d380a0f75f1b97333f65
Kenny <adelekekehinde06@gmail.com>
2023-07-17 21:26:24 +0100

    style: width changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,24 @@
+import { Box, Container } from "@mui/material"
 import { BottomMenu, Header } from "components"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <>
+    <Box sx={{
+      lg: {
+        maxWidth: '40rem'
+      },
+      margin: '0 auto'
+    }}>
       <Header />
-      <Outlet />
+      <Box sx={{
+        maxWidth: '40rem',
+        margin: '0 auto'
+      }}>
+        <Outlet />
+      </Box>
       <BottomMenu />
-    </>
+    </Box>
   )
 }
 
